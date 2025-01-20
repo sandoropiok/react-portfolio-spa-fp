6907b6539abaeed0a60633873ae587b9fa2895ab
Sandor Opiok <sandor.opiok@dci-student.org>
2025-01-20 16:05:53 +0100

    second section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
     </>
   );
 }
